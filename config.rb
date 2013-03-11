362df984f6d1e4cfacc9f9d4940daa0680664bd6
Alexander Murashov <rodger@ladela.com>
2013-03-11 10:25:23 +0200

    tune compass tracking

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "assets/sass"
+sass_dir = "assets/scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
