fe8d624960c36c1e2910975a57d105dcdf415a88
Alexander Murashov <rodger@ladela.com>
2013-03-11 11:02:43 +0200

    add backbone, define models

---
+++ javascripts/engine.js
@@ -0,0 +1,5 @@
+// engine file
+
+$(function() {
+  console.log('aaa');
+});
---
+++ javascripts/model/models.js
@@ -0,0 +1,49 @@
+/**
+ * Edge definition
+ * @type {Backbone.Model.extend}
+ */
+var Edge = new Backbone.Model.extend({
+  defaults:{
+    id                         :null,
+    from_node_id               :null,
+    to_node_id                 :null,
+    distance                   :null,
+    surface_id                 :null,
+    average_velocity_ascending :null,
+    average_velocity_descending:null,
+    frequency                  :null,
+    frequency_ascending        :null,
+    lat_min                    :null,
+    lat_max                    :null,
+    lng_min                    :null,
+    lng_max                    :null,
+    ascent                     :null,
+    descent                    :null,
+    edge_rank                  :null,
+    num_clicks                 :null
+  }
+});
+
+/**
+ * Edges collection definition
+ * @type {Backbone.Model.extend}
+ */
+var EdgesCollection = new Backbone.Collection.extend({model:Edge});
+
+/**
+ * Point definition
+ * @type {Backbone.Model.extend}
+ */
+var Point = new Backbone.Model.extend({
+  defaults: {
+    id: null,
+    lat: null,
+    lng: null
+  }
+});
+
+/**
+ * Points collection definition
+ * @type {Backbone.Model.extend}
+ */
+var PointsCollection = new Backbone.Collection.extend({model:Point});
